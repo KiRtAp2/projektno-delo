4182aebd493db93799072cd5870bff73ce6f0596
no <posta.patrik@gmail.com>
2018-09-17 17:14:12 +0200

    CSS v svojem direktoriju

--- koda/main.py
+++ koda/main.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from flask import render_template
-from flask import request
+from flask import render_template, request, send_from_directory
+
 
 app = Flask(__name__)
 app.secret_key = "akshfdgas"
@@ -22,6 +22,13 @@ def index():
     spojine=["a","bb","c"]
     # spojine je list spojin
     return render_template("domaca_stran.html", spojine=spojine, prou=prou)
+
+
+# TEGA SE NE DELA V PRODUCTIONU - TO JE SAMO ZA DEBUG
+@app.route("/static/<path:path>")
+def send_static(path):
+    return send_from_directory('static', path)
+    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
