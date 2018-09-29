4ed7e7df56dddad79898c82727c66124a2c4944d
no <posta.patrik@gmail.com>
2018-09-29 21:30:35 +0200

    Dodani elementi

--- koda/models.py
+++ koda/models.py
@@ -9,7 +9,17 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(256), nullable=False)
     admin = db.Column(db.Boolean, nullable=False)
 
+    
 class Scores(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     score = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+
+    
+class Element(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    simbol = db.Column(db.String(3), unique=True, nullable=False)
+    ime = db.Column(db.String(20), unique=True, nullable=False)
+
+    def __repr__(self):
+        return "<Element {}>".format(self.simbol)
