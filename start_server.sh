8f609c516bec397f5ca523f0ddcc9bb4f257058b
no <posta.patrik@gmail.com>
2019-04-15 13:48:47 +0200

    nepotrebne datoteke

--- start_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export OAUTHLIB_INSECURE_TRANSPORT=1
-export YOURAPPLICATION_SETTINGS=../config.cfg
-python3 koda/main.py
--- updatedb.cmd
+++
@@ -1,3 +0,0 @@
-SET YOURAPPLICATION_SETTINGS=..\config.cfg
-python modify_base.py --file scratch.txt
-python koda\main.py --db-create-all
